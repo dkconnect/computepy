74f47268af1efe11713460a4aea524df3055dc86
Lucifer <dkroyals1001@gmail.com>
2026-08-04 10:00:18 +0530

    Create powers.py

---
+++ basics/powers.py
@@ -0,0 +1,28 @@
+"""
+powers.py
+
+=============
+Power Functions
+=============
+
+Author: Dibyanshu | August 2026
+"""
+
+#---- Basic Powers ----
+def power(base, exponent):
+    return base ** exponent
+
+def square(x):
+    return x * x
+
+def cube(x):
+    return x * x * x
+
+def fourth_power(x):
+    return x ** 4
+
+def fifth_power(x):
+    return x ** 5
+
+def nth_power(base, n):
+    return base ** n
