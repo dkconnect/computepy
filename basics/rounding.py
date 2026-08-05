888b8ae6fbc48965696aba91c272af7b17254353
Lucifer <dkroyals1001@gmail.com>
2026-08-05 08:07:52 +0530

    Starting with Basic Roundings

--- basics/rounding.py
+++ basics/rounding.py
@@ -1,1 +1,56 @@
+"""
+rounding.py
 
+=============
+Rounding Functions
+=============
+
+Author: Dibyanshu | August 2026
+"""
+
+#---- Basic Rounding ----
+def floor(number):
+    """
+    Returns the greatest integer less than or equal to the number.
+    """
+
+    integer = int(number)
+    if number >= 0 or number == integer:
+        return integer
+    return integer - 1
+  
+
+def ceil(number):
+    """
+    Returns the smallest integer greater than or equal to the number.
+    """
+
+    integer = int(number)
+
+    if number == integer:
+        return integer
+
+    if number > 0:
+        return integer + 1
+
+    return integer
+
+
+def truncate(number):
+    """
+    Removes the decimal part.
+    """
+
+    return int(number)
+
+
+def round(number):
+    """
+    Rounds to the nearest integer.
+    Half values are rounded away from zero.
+    """
+
+    if number >= 0:
+        return floor(number + 0.5)
+
+    return ceil(number - 0.5)
